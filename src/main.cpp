11f5367104827e2bc8dab7f35a575b183a41650a
lennom <lennom@gmail.com>
2011-12-07 03:46:38 -0200

    adicionei arquivo interface.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include "interface.hpp"
+#include "StackFrame.hpp"
+
 int main()
 {
+    StackFrame testedocaralho;
+    testedocaralho.data.push_back(Entry(ENTRY_PARAMETER,1,"short Eita"));
+    testedocaralho.data.push_back(Entry(ENTRY_PARAMETER,4,"int Eita2"));
+
+    testedocaralho.data.push_back(Entry(ENTRY_VARIABLES,1,"short var1"));
+    testedocaralho.data.push_back(Entry(ENTRY_VARIABLES,4,"int var2"));
+    printTable(testedocaralho);
+    getchar();
     return 0;
 }
