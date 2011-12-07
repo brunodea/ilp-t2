e789e124b21c141463f12e8a65d48eee4cdc6b0f
brunodea <brunodea@inf.ufsm.br>
2011-12-07 05:09:54 -0200

    arrumei alguns problemas com o projeto e com a main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
 #include "interface.hpp"
 #include "StackFrame.hpp"
+#include "parser.hpp"
 
 int main()
 {
     StackFrame testedocaralho;
     testedocaralho.data.push_back(Entry(ENTRY_PARAMETER,1,"short Eita"));
-    testedocaralho.data.push_back(Entry(ENTRY_PARAMETER,4,"int Eita2"));
+    testedocaralho.data.push_back(Entry(ENTRY_PARAMETER,1,"short Eita2"));
 
     testedocaralho.data.push_back(Entry(ENTRY_VARIABLES,1,"short var1"));
     testedocaralho.data.push_back(Entry(ENTRY_VARIABLES,4,"int var2"));
     printTable(testedocaralho);
     getchar();
-=======
-#include "parser.hpp"
 
-int main()
-{
-    parse::test_parse();
->>>>>>> a7b072536f8248aa56ef2512de7a7c44973878ad
     return 0;
 }
