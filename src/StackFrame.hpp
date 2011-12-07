da9d87227ffc2c8ebf91be02238365a8973d1c1f
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-12-07 01:53:43 -0200

    Changing StackFrame entry type.

--- src/StackFrame.hpp
+++ src/StackFrame.hpp
@@ -3,12 +3,19 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <tuple>
+
+enum EntryType  {
+    ENTRY_PARAMETER, ENTRY_STATIC_LINK, ENTRY_RETURN_ADRESS, ENTRY_DYNAMIC_LINK,
+    ENTRY_SAVED_CONTEXT, ENTRY_TEMPORARY, ENTRY_VARIABLES
+};
+
+
+typedef std::tuple<EntryType, size_t, std::string> Entry;
 
 struct StackFrame {
     //parametros, elo estatico, end. retorno, elo dinamico, contexto salvo,
     //temporarios, variaveis
 
-    typedef std::pair<size_t, std::string>  EntryType;
-    
-    std::vector<EntryType> data; 
+    std::vector<Entry> data; 
 };
